99192a916b83f2cdef4cfb7cbc76ef229459248c
Defectfinder <mkalpana705@gmail.com>
2024-12-12 09:31:12 +1300

    initial Commit

---
+++ Nunit_proj/Pages/EducationTab.cs
@@ -0,0 +1,53 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nunit_proj.Pages
+{
+    public class EducationTab
+    {
+        public void  AddEducation(IWebDriver driver) {
+            
+            IWebElement educationTab = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[1]/a[3]"));
+            educationTab.Click();
+            Thread.Sleep(4000);
+
+            IWebElement addEducationButton = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[4]/div/div[2]/div/table/thead/tr/th[6]/div"));
+            addEducationButton.Click();
+
+            IWebElement collegeTextbox = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[4]/div/div[2]/div/div/div[1]/div[1]/input"));
+            collegeTextbox.SendKeys("Pondicherry University");
+            Thread.Sleep(3000);
+
+            IWebElement countryDropdown = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[4]/div/div[2]/div/div/div[1]/div[2]/select"));
+            collegeTextbox.Click();
+
+            IWebElement optionSelect = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[4]/div/div[2]/div/div/div[1]/div[2]/select/option[66]"));
+            optionSelect.Click();
+
+            IWebElement titleDropdown = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[4]/div/div[2]/div/div/div[2]/div[1]/select"));
+            titleDropdown.Click();
+            Thread.Sleep(2000);
+
+            IWebElement optionTitleSelect = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[4]/div/div[2]/div/div/div[2]/div[1]/select/option[6]"));
+            optionTitleSelect.Click();
+            Thread.Sleep(3000);
+
+            IWebElement degreeText = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[4]/div/div[2]/div/div/div[2]/div[2]/input"));
+            degreeText.SendKeys("HR");
+
+            IWebElement yearDropdown = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[4]/div/div[2]/div/div/div[2]/div[3]/select"));
+            yearDropdown.Click();
+
+            IWebElement yearSelect = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[4]/div/div[2]/div/div/div[2]/div[3]/select/option[11]"));
+            yearSelect.Click();
+
+            IWebElement addButton = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[4]/div/div[2]/div/div/div[3]/div/input[1]"));
+            addButton.Click();
+
+        }
+    }
+}
---
+++ Nunit_proj/Tests/LoginTest.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nunit_proj.Pages;
+using Nunit_proj.Utilities;
+
+namespace Nunit_proj.Tests
+{
+    [TestFixture]
+    public class LoginTest : CommonDriver
+    {
+        [SetUp]
+        public void SetUpSteps()
+        {
+
+            driver = new ChromeDriver();
+
+            // Login page object initialization and definition
+            Login loginPageObj = new Login();
+            loginPageObj.LoginActions(driver);
+
+            // Profile page object intialization and definition
+            ProfilePage profilePageObj = new ProfilePage();
+            profilePageObj.NavigateToProfile(driver);
+        }
+        [Test]
+       public void AddEducation_Test() 
+        {
+            EducationTab educationPageObj = new EducationTab();
+            educationPageObj.AddEducation(driver);
+
+
+
+
+        }
+        
+       
+    }
+}
